10b8c37c196acfe447a0fdb7098866e1c6a1ea2f
daeuun <leeda51925@gmail.com>
2022-01-23 23:50:18 +0900

    controller생성

--- src/main/java/com/de/project/DemoModel.java
+++ src/main/java/com/de/project/DemoModel.java
@@ -1,5 +1,13 @@
 package com.de.project;
 
-public class DemoModel {
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
+@Builder
+@RequiredArgsConstructor
+public class DemoModel {
+	
+	@NonNull
+	private String id;
 }
