f7a70601578978910d3f0b835ec207b8b9d2e27e
daeuun <leeda51925@gmail.com>
2022-01-30 00:01:01 +0900

    Update TodoController.java

--- src/main/java/com/de/project/controller/TodoController.java
+++ src/main/java/com/de/project/controller/TodoController.java
@@ -6,10 +6,11 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.de.project.dto.ResponseDTO;
 
-@ResponseController
+@RestController
 @RequestMapping("todo")
 public class TodoController {
 	
@@ -17,7 +18,7 @@ public class TodoController {
 	public ResponseEntity<?> todoControllerResponseEntity() {
 		List<String> list = new ArrayList<>();
 		list.add("todo ResponseEntity");
-		responseDTO<String> response = ResponseDTO.builder().<String>data(list).build();
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.badRequest().body(response);
 	}
 
