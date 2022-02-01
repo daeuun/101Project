b61ca3fc18a95acc5d09ccc61f8281f25c3231e2
daeuun <leeda51925@gmail.com>
2022-02-01 23:59:01 +0900

    controller 구현

--- src/main/java/com/de/project101/controller/TodoController.java
+++ src/main/java/com/de/project101/controller/TodoController.java
@@ -2,14 +2,19 @@ package com.de.project101.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.de.project101.dto.ResponseDTO;
+import com.de.project101.dto.TodoDTO;
+import com.de.project101.model.TodoEntity;
 import com.de.project101.service.TodoService;
 
 @RestController
@@ -26,6 +31,39 @@ public class TodoController {
 		list.add(str);
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+		try {
+			String temporaryUserId = "temporary-user"; // temporary user id
+			
+			// (1) TodoEntity로 반환한다.  
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// (2) id를 null로 초기화한다. 생성 당시에는 id가 없어야하기 때문
+			entity.setId(null);
+			
+			// (3) 임시 사용자 아이디 설정.  
+			entity.setUserId(temporaryUserId);
+			
+			// (4) 서비스를 이용해 Todo 엔티티 생성
+			List<TodoEntity> entities = service.create(entity);
+			
+			// (5) 엔티티 리스트 => TodoDTO 리스트로 변환 
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			// (6) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// (7) ResponseDTO를 리턴
+			return ResponseEntity.ok().body(response);
+			
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 
 
--- src/main/java/com/de/project101/persistence/TodoRepository.java
+++ src/main/java/com/de/project101/persistence/TodoRepository.java
@@ -3,7 +3,6 @@ package com.de.project101.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.de.project101.model.TodoEntity;
@@ -12,9 +11,9 @@ import com.de.project101.model.TodoEntity;
 public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	
 	// ?1 은 메서드의 매개변수의 순서 위치  
-	@Query("select * from Todo t where t.userId = ?1")
-	List<TodoEntity> findByUserId(String userId);
+	//@Query("select * from Todo t where t.userId = ?1")
+	//List<TodoEntity> findByUserId(String userId);
 	
-
+	List<TodoEntity> findByUserId(String userId);
 	
 }
