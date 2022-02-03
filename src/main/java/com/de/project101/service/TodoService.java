39e97fb2e3ecda505837489231ed4e76eab3430c
daeuun <leeda51925@gmail.com>
2022-02-03 23:41:22 +0900

    delete 처리

--- src/main/java/com/de/project101/service/TodoService.java
+++ src/main/java/com/de/project101/service/TodoService.java
@@ -77,4 +77,23 @@ public class TodoService {
 		// retrieve Todo에서 만든 메서드를 이용해 사용자의 모든 Todo 리스트를 리턴한다.
 		return retrieve(entity.getUserId());
 	}
+	
+	public List<TodoEntity> delete(final TodoEntity entity) {
+		// (1) 저장할 엔티티가 유효한지 확인한다.  
+		validate(entity);
+		
+		try {
+			// (2) 엔티티를 삭제한다. 
+			repository.delete(entity);
+		} catch(Exception e) {
+			// (3) exceptio 발생시 로깅  
+			log.error("error deleting entity", entity.getId(), e);
+			
+			// (4) 컨트롤러로 exception을 보낸다. 디비 내부 로직을 캡슐화하기 위해 e를 리턴하지 않고 새로운 exception 오브젝트를 리턴한다.
+			throw new RuntimeException("error deleting entity " + entity.getId());
+		}
+		
+		// (5) 새 Todo 리스트를 가져와 리턴한다. 
+		return retrieve(entity.getUserId());
+	}
 }
